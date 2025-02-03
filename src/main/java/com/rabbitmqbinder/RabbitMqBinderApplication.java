c4ea830000b0251b0548483febb557713d648097
owen <floralexium@gmail.com>
2025-02-03 09:54:41 +0900

    init

---
+++ src/main/java/com/rabbitmqbinder/RabbitMqBinderApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmqbinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqBinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqBinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rabbitmqbinder/RabbitMqBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmqbinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqBinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
